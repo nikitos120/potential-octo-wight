d19e5b26d48c5b94776a14a226e35fc1cc5495bb
nik <nikitos120@mail.ru>
2013-06-22 14:26:05 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ycheba::Application.config.secret_token = '7d282a565cb980b3c599e0d2500bd2dc1ec6d9d75ce8bbdf6f3d72e1b055c7336c93dca46439dc0d0fc0ff880ca184bd2d46b6c877c10b8cd34c10b18a1686b6'
